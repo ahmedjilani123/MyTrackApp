61e71c1f4af81f98c80f14e8dc5d9277d7785df0
ahmedjilani123 <ahmedjilani0011@gmail.com>
2025-10-06 16:58:17 +0530

    add login page

--- webapp/controller/Initial.controller.js
+++ webapp/controller/Initial.controller.js
@@ -5,6 +5,14 @@ sap.ui.define([
 
     return Controller.extend("aj.sap.myexpenseapp.controller.Initial", {
         onInit() {
+              
+        },
+        RouterHandler(){
+            var Router = this.getOwnerComponent().getRouter();
+            Router.navTo("RouteDashboard");
+        },
+        submit(ovalue){
+            console.log(ovalue)
         }
     });
 });
